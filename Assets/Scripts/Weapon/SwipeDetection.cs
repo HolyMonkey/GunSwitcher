4dd4ac2812e430e1db890157593f46b9193a9ab1
Nikolai <nikolajmenkov50@gmail/com>
2021-10-13 01:24:01 +0300

    Assault, change weapon

--- Assets/Scripts/Weapon/SwipeDetection.cs
+++ Assets/Scripts/Weapon/SwipeDetection.cs
@@ -3,5 +3,109 @@ using UnityEngine;
 
 public class SwipeDetection : MonoBehaviour
 {
-    public Action<Vector2> 
+    public static event OnSwipeInput SwipeEvent;
+    public delegate void OnSwipeInput(Vector2 direction);
+
+    private Vector2 _tapPosition;
+    private Vector2 _swipeDelta;
+
+    [SerializeField] private float _deadZone = 80;
+
+    [SerializeField] private bool _isSwiping;
+    [SerializeField] private bool _isMobile;
+
+    private void Start()
+    {
+        _isMobile = Application.isMobilePlatform;
+    }
+
+    private void Update()
+    {
+        if (_isMobile == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Debug.Log("давн");
+                _isSwiping = true;
+                _tapPosition = Input.mousePosition;
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                // Debug.Log("ап");
+                ResetSwipe();
+            }
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                    _isSwiping = true;
+                    _tapPosition = Input.GetTouch(0).position;
+                }
+                else if (Input.GetTouch(0).phase == TouchPhase.Canceled || Input.GetTouch(0).phase == TouchPhase.Ended)
+                {
+                    ResetSwipe();
+                }
+            }
+        }
+        
+        // Debug.Log(_isSwiping);
+        CheckSwipe();
+    }
+
+    private void CheckSwipe()
+    {
+        // Debug.Log("CheckSwipe");
+        
+        _swipeDelta = Vector2.zero;
+
+        Vector2 direction;
+        
+        if (_isSwiping)
+        {
+            if (_isMobile == false && Input.GetMouseButton(0))
+            {
+                _swipeDelta = (Vector2)Input.mousePosition - _tapPosition;
+            }
+            else if (Input.touchCount > 0)
+            {
+                _swipeDelta = Input.GetTouch(0).position - _tapPosition;
+            }
+        }
+
+        if (_swipeDelta.magnitude > _deadZone)
+        {
+            if (SwipeEvent != null)
+            {
+                if (Mathf.Abs(_swipeDelta.x) > Mathf.Abs(_swipeDelta.y))
+                {
+                    direction = _swipeDelta.x > 0 ? Vector2.right : Vector2.left;
+                }
+                else
+                {
+                    direction = _swipeDelta.y > 0 ? Vector2.up : Vector2.down;
+                }
+                
+                SwipeEvent?.Invoke(direction);
+                DenugSWIPES(direction);
+            }
+            
+            ResetSwipe();
+        }
+    }
+
+    private void DenugSWIPES(Vector2 direction)
+    {
+        Debug.Log(direction);
+    }
+    
+    private void ResetSwipe()
+    {
+        _isSwiping = false;
+        
+        _tapPosition = Vector2.zero;
+        _swipeDelta = Vector2.zero;
+    }
 }
--- Assets/Scripts/Weapon/SwitchGun.cs
+++ Assets/Scripts/Weapon/SwitchGun.cs
@@ -10,8 +10,71 @@ public class SwitchGun : MonoBehaviour
     [SerializeField] private Image _rightGun;
     [SerializeField] private List<Weapon> _weapons;
 
-    [SerializeField] private int _currentWeaponIndex;
+    // [SerializeField] private SwipeDetection _swipeDetection;
     
+    [SerializeField] private int _currentWeaponIndex;
+
+    private void Start()
+    {
+        Debug.Log(_weapons.Count);
+    }
+
+    private void OnEnable()
+    {
+        SwipeDetection.SwipeEvent += OnSwipe;
+    }
+
+    private void OnDisable()
+    {
+        SwipeDetection.SwipeEvent -= OnSwipe;
+    }
+
+    private void OnSwipe(Vector2 direction)
+    { 
+        if(direction == Vector2.left)
+        {
+            ChangeWeapon(false);
+        }
+        else if (direction == Vector2.right)
+        {
+            ChangeWeapon(true);
+        }
+    }
+
+    private void ChangeWeapon(bool isNext)
+    {
+        if (isNext)
+        {
+            _currentWeaponIndex++;
+        }
+        else
+        {
+            _currentWeaponIndex--;
+        }
+
+        if (_currentWeaponIndex > _weapons.Count - 1)
+        {
+            _currentWeaponIndex = 0;
+        }
+        else if(_currentWeaponIndex < 0)
+        {
+            _currentWeaponIndex = _weapons.Count - 1;
+        }
+        
+        DrawIcons();
+    }
+
+    private void DrawIcons()
+    {
+        int left = _currentWeaponIndex == 0 ? _weapons.Count - 1 : _currentWeaponIndex - 1;
+        int right = _currentWeaponIndex == _weapons.Count -1 ? 0 : _currentWeaponIndex + 1;
+
+        Debug.Log($"Left {left}, Right {right}");
+        
+        _leftGun.sprite = _weapons[left].Icon;
+        _rightGun.sprite = _weapons[right].Icon;
+    }
+
     [Serializable]
     public class Weapon
     {
