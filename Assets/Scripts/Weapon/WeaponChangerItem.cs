75e2fbdfe4ed76ceed667f8564ce711e378e5f3b
LENOVO <LENOVO@LAPTOP-3K0UVCAK>
2021-11-08 12:28:24 +0200

    create effects for enemies and guns

--- Assets/Scripts/Weapon/WeaponChangerItem.cs
+++ Assets/Scripts/Weapon/WeaponChangerItem.cs
@@ -1,11 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Weapon
 {
     public class WeaponChangerItem : MonoBehaviour
     {
         [SerializeField] private Weapons _id;
+        [SerializeField] private GameObject _zoneEffect;
+        [SerializeField] private Transform _effectParent;
 
+        private GameObject _zone;
+        
         public Weapons Id => _id;
+
+        private void OnEnable()
+        {
+            _zone = Instantiate(_zoneEffect, transform.position, Quaternion.identity, _effectParent);
+        }
+
+        private void OnDisable()
+        {
+            Destroy(_zone);
+        }
     }
 }
