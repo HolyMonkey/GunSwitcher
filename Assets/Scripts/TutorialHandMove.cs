8c9ef103396b4c24ec7b0c54c9873c44a2b0a7d4
LENOVO <LENOVO@LAPTOP-3K0UVCAK>
2021-12-24 01:58:44 +0200

    UI update

---
+++ Assets/Scripts/TutorialHandMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialHandMove : MonoBehaviour
+{
+    [SerializeField] private Transform _pointA;
+    [SerializeField] private Transform _pointB;
+    [SerializeField] private float _speed;
+
+    private Transform _target;
+
+    private void Start()
+    {
+        _target = _pointA;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
+
+        if (transform.position == _pointB.position)
+        {
+            _target = _pointA;
+        }
+        else if (transform.position == _pointA.position)
+        {
+            _target = _pointB;
+        }
+    }
+}
