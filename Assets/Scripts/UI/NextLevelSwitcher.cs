184e7dd5bbe31224ab19ad6deda997afb12ab844
LENOVO <LENOVO@LAPTOP-3K0UVCAK>
2022-01-19 12:46:45 +0200

    update ui 1

--- Assets/Scripts/UI/NextLevelSwitcher.cs
+++ Assets/Scripts/UI/NextLevelSwitcher.cs
@@ -1,15 +1,30 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NextLevelSwitcher : MonoBehaviour
 {
    [SerializeField] private int _currentLevel;
-   
+   [SerializeField] private TMP_Text _currentLevelCounter;
+   [SerializeField] private TMP_Text _nextLevelCouner;
+
+   private int _currentLevelCount = 1;
+   private int _nextLevelCount => _currentLevelCount + 1;
    private int _maxLevel = 10;
 
    private void Awake()
    {
-      Debug.Log(PlayerPrefs.GetInt("level"));
+      if (PlayerPrefs.GetInt("levelCount") == 0)
+      {
+         _currentLevelCount = 1;
+      }
+      else
+      {
+         _currentLevelCount = PlayerPrefs.GetInt("levelCount");
+      }
+
+      _currentLevelCounter.text = _currentLevelCount.ToString();
+      _nextLevelCouner.text = _nextLevelCount.ToString();
       
       if (_currentLevel != PlayerPrefs.GetInt("level"))
       {
@@ -26,14 +41,17 @@ public class NextLevelSwitcher : MonoBehaviour
    {
       if (_currentLevel + 1 >= _maxLevel)
       {
-         _currentLevel = 0;
+         _currentLevel = 4;
       }
       else
       {
          _currentLevel++;
       }
 
+      _currentLevelCount++;
+
       PlayerPrefs.SetInt("level", _currentLevel);
+      PlayerPrefs.SetInt("levelCount", _currentLevelCount);
       SceneManager.LoadScene(_currentLevel);
    }
 }
